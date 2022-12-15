d07db392e9d7857e3338945bbddf2919714c06bc
Esteban Ahumada <esteban.manuel.ahumada@gmail.com>
2022-12-14 22:16:33 -0300

    Se agrego CrossOrigin

--- src/main/java/com/company/inventory/controller/CategoryRestController.java
+++ src/main/java/com/company/inventory/controller/CategoryRestController.java
@@ -1,7 +1,9 @@
 package com.company.inventory.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +17,8 @@ import com.company.inventory.model.Category;
 import com.company.inventory.response.CategoryResponseREST;
 import com.company.inventory.services.ICategoryService;
 
+
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api/v1")
 public class CategoryRestController {
