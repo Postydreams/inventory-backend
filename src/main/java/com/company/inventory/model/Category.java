de3d0aad7ecaaf478e7f23dade757ac351fd39e0
Esteban Ahumada <epunamun_@hotmail.com>
2023-01-13 11:30:13 -0300

    Se establecio en el modelo de Producto las variables y la relacion de muchos a uno

--- src/main/java/com/company/inventory/model/Category.java
+++ src/main/java/com/company/inventory/model/Category.java
@@ -23,7 +23,9 @@ public class Category implements Serializable{
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private String name;
+	
 	private String description;
 	
 
--- src/main/java/com/company/inventory/model/Product.java
+++ src/main/java/com/company/inventory/model/Product.java
@@ -1,5 +1,44 @@
 package com.company.inventory.model;
 
-public class Product {
+import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="product")
+public class Product implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7461389651533509262L; 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	
+	private int price;
+	
+	private int quantity;
+	
+	@ManyToOne(fetch= FetchType.LAZY)
+	@JsonIgnoreProperties ({"hibernateLazyInitializer", "handler"})
+	private Category category;
+	
+	@Column(name="picture", length = 1000) 
+	private byte[] picture;
+	
 }
