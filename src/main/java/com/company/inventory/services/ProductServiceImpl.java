9860c54734913b0522d57a8a4f42a313e2274bd0
Esteban Ahumada <epunamun_@hotmail.com>
2023-01-13 14:32:12 -0300

    Se agrego servicio que busca por nombre

--- src/main/java/com/company/inventory/services/ProductServiceImpl.java
+++ src/main/java/com/company/inventory/services/ProductServiceImpl.java
@@ -111,4 +111,46 @@ public class ProductServiceImpl implements IProductService {
 		return new ResponseEntity<ProductResponseRest>(response, HttpStatus.OK);
 	}
 
-}
+
+	@Override
+	@Transactional(readOnly=true)
+	public ResponseEntity<ProductResponseRest> searchByName(String name) {
+		ProductResponseRest response = new ProductResponseRest();
+		List<Product> list = new ArrayList<>();
+		List<Product> listAux = new ArrayList<>();
+		
+		try {
+			
+			// Busca producto por nombre
+			listAux = productDao.findByNameContainingIgnoreCase(name);
+			
+			
+			
+			if (listAux.size() > 0) {
+				
+				listAux.stream().forEach( (p) ->{
+					byte[] imageDescompressed = Util.decompressZLib(p.getPicture());
+					p.setPicture(imageDescompressed);
+					list.add(p);
+				});
+	
+				response.getProduct().setProducts(list);
+				response.setMetadata("Respuesta ok", "00", "Productos encontrado");
+				
+			} else {
+				response.setMetadata("Respuesta nok", "-1", "Productos no encontrado");
+				return new ResponseEntity<ProductResponseRest>(response, HttpStatus.NOT_FOUND);
+			}
+			
+			
+		} catch (Exception e) {
+			e.getStackTrace();
+			response.setMetadata("Respuesta nok", "-1", "Error al buscar producto por nombre");
+			return new ResponseEntity<ProductResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+			
+		}
+		
+		return new ResponseEntity<ProductResponseRest>(response, HttpStatus.OK);
+	}
+	}
+
